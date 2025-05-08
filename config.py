b8ebea146bfe110847c8d87987ba0fdbcc47e4c8
atetheone <atevirran@gmail.com>
2025-05-08 12:54:30 +0000

    Testing env settings

--- config.py
+++ config.py
@@ -3,19 +3,29 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-for-testing'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///bookstore.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
 
 class ProductionConfig(Config):
     DEBUG = False
 
+
 config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
+    'testing': TestingConfig,
     'default': DevelopmentConfig
 }
