0834772f6c34a20588e6a80a4ac1c6ad86dc57d9
atetheone <atevirran@gmail.com>
2025-05-09 10:49:22 +0000

    Uncomment category routes in the route registration module

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,7 +3,7 @@ This module is responsible for registering all the route blueprints"""
 
 from app.routes.authors import authors_bp
 from app.routes.books import books_bp
-# from app.routes.categories import categories_bp
+from app.routes.categories import categories_bp
 # from app.routes.publishers import publishers_bp
 
 
@@ -16,5 +16,5 @@ def register_routes(app):
     """
     app.register_blueprint(authors_bp, url_prefix="/api/authors")
     # app.register_blueprint(publishers_bp, url_prefix="/api/publishers")
-    # app.register_blueprint(categories_bp, url_prefix="/api/categories")
+    app.register_blueprint(categories_bp, url_prefix="/api/categories")
     app.register_blueprint(books_bp, url_prefix="/api/books")
