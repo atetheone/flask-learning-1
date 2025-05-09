22e68cbafcadebb0b0a4ddeff56302de0e27ec3e
atetheone <atevirran@gmail.com>
2025-05-09 11:46:46 +0000

    Uncomment model imports in the models package for accessibility

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,10 @@
 # Import models to make them available from the models package
-# from app.models.author import Author
-# from app.models.book import Book
-# from app.models.category import Category
-# from app.models.publisher import Publisher
+from app.models.author import Author
+from app.models.book import Book
+from app.models.category import Category
+from app.models.publisher import Publisher
+
+Author = Author
+Book = Book
+Category = Category
+Publisher = Publisher
