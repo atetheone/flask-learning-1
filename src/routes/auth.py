c139b31f6845d5566f7ffd60c6a0bc366c7f9901
atetheone <atevirran@gmail.com>
2025-05-16 12:45:13 +0000

    Remove unused route files for authentication, users, posts, and comments

--- src/routes/auth.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint  # , request, jsonify
-
-auth_bp = Blueprint('auth', __name__)
--- src/routes/comments.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint   # , request, jsonify
-
-comments_bp = Blueprint('comments', __name__)
